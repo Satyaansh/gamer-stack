e7d5d0ae88252d95958315725cb92cf5643bc4a2
Satyaansh <satyanshasthana@gmail.com>
2023-03-20 21:54:36 +0530

    initial commit

---
+++ src/main/java/com/projects/gamerstack/GamerStackApplication.java
@@ -0,0 +1,13 @@
+package com.projects.gamerstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/gamerstack/GamerStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.gamerstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
