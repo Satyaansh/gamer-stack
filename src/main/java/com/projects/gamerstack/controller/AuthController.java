5c471fef249392a87acf99e930df4f220d4619e2
Satyaansh <satyanshasthana@gmail.com>
2023-04-12 17:48:09 +0530

    Add AuthController for signup API

---
+++ src/main/java/com/projects/gamerstack/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.projects.gamerstack.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projects.gamerstack.dto.RegisterRequest;
+import com.projects.gamerstack.service.AuthService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    
+}
