809f40533ef4f2362879469b525988ffabb0eab7
Satyaansh <satyanshasthana@gmail.com>
2023-04-12 17:51:33 +0530

    Add AuthService for signup logic impl

---
+++ src/main/java/com/projects/gamerstack/service/AuthService.java
@@ -0,0 +1,59 @@
+package com.projects.gamerstack.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projects.gamerstack.dto.RegisterRequest;
+import com.projects.gamerstack.model.User;
+import com.projects.gamerstack.model.VerificationToken;
+import com.projects.gamerstack.repository.UserRepository;
+import com.projects.gamerstack.repository.VerificationTokenRepository;
+import com.projects.gamerstack.util.Constants;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailContentBuilder mailContentBuilder;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token  = generateVerificationToken(user);
+        String message = mailContentBuilder.build("Thank You for signing up for GamerStack! Please click on this URL to activate your account : "
+                     + Constants.ACTIVATION_EMAIL + "/" + token);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+    
+}
