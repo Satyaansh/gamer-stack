5c7af53fb6390522951385019e7721f1fd5ceca1
Satyaansh <satyanshasthana@gmail.com>
2023-04-13 17:54:21 +0530

    Add MailService (mail sending logic)

---
+++ src/main/java/com/projects/gamerstack/service/MailService.java
@@ -0,0 +1,40 @@
+package com.projects.gamerstack.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.projects.gamerstack.exception.GamerStackException;
+import com.projects.gamerstack.model.NotificationEmail;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom("gamerstack@email.com");
+            mimeMessageHelper.setTo(notificationEmail.getRecipient());
+            mimeMessageHelper.setSubject(notificationEmail.getSubject());
+            mimeMessageHelper.setText(notificationEmail.getBody());
+        };
+        try {
+            javaMailSender.send(mimeMessagePreparator);
+            log.info("Activation Email sent!!");
+        }
+        catch (MailException e) {
+            throw new GamerStackException("Exception occurred when sending mail to " + notificationEmail.getRecipient());
+        }
+    }
+    
+}
