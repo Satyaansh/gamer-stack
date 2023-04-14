e16189f2c66a8402bb2ea9479d4de89f66242ba4
Satyaansh <satyanshasthana@gmail.com>
2023-04-14 16:21:06 +0530

    Switch to Asynchronus Mail sending

--- src/main/java/com/projects/gamerstack/GamerStackApplication.java
+++ src/main/java/com/projects/gamerstack/GamerStackApplication.java
@@ -2,8 +2,10 @@ package com.projects.gamerstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class GamerStackApplication {
 
 	public static void main(String[] args) {
