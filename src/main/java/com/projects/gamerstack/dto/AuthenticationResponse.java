be5b8a63a90cd3b1b3a238793ffe733590cdfa23
Satyaansh <satyanshasthana@gmail.com>
2023-04-19 17:29:54 +0530

    Add Authentication response dto

---
+++ src/main/java/com/projects/gamerstack/dto/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.projects.gamerstack.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String username;
+}
