acd910bec36eb100fe544404f5c28239aa675f72
Satyaansh <satyanshasthana@gmail.com>
2023-04-24 17:34:50 +0530

    Create Subreddit Controller

---
+++ src/main/java/com/projects/gamerstack/controller/SubredditController.java
@@ -0,0 +1,36 @@
+package com.projects.gamerstack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projects.gamerstack.model.Subreddit;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/subreddit")
+@AllArgsConstructor
+public class SubredditController {
+
+    private final SubredditService subredditService;
+
+    @GetMapping
+    public List<SubredditDto> getAllSubreddits() {
+        return subredditService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public SubredditDto getSubreddit(@PathVariable long id) {
+        return subredditService.getSubreddit(id);
+    }
+
+    @PostMapping
+    public SubredditDto create(@RequestBody @Valid SubredditDto subredditDto) {
+        return subredditService.save(subredditDto);
+    }
+    
+}
