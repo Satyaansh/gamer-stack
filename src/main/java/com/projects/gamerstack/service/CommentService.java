43308602cdc27dec82c65ce777694f6ba81c3d7e
Satyaansh <satyanshasthana@gmail.com>
2023-05-17 18:41:34 +0530

    Add Comment service

---
+++ src/main/java/com/projects/gamerstack/service/CommentService.java
@@ -0,0 +1,15 @@
+package com.projects.gamerstack.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
+public class CommentService {
+    
+}
