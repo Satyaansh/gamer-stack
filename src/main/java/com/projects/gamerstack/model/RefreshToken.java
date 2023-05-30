9c96116b040dda3a2e202fdd760a0d445b8395c1
Satyaansh <satyanshasthana@gmail.com>
2023-05-30 11:31:16 +0530

    Add refresh token model and respository

---
+++ src/main/java/com/projects/gamerstack/model/RefreshToken.java
@@ -0,0 +1,26 @@
+package com.projects.gamerstack.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Instant createdDate;
+}
