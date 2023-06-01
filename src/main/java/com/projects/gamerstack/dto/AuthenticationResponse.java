13210dd2d67c462f8d9e43e909f0aa1c9e9a1a47
Satyaansh <satyanshasthana@gmail.com>
2023-06-01 21:52:06 +0530

    Edit Auth response for refresh token

--- src/main/java/com/projects/gamerstack/dto/AuthenticationResponse.java
+++ src/main/java/com/projects/gamerstack/dto/AuthenticationResponse.java
@@ -1,11 +1,17 @@
 package com.projects.gamerstack.dto;
 
+import java.time.Instant;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AuthenticationResponse {
     private String authenticationToken;
     private String username;
+    private String refreshToken;
+    private Instant expiresAt;
 }
